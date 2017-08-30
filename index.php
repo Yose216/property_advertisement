a5b8127708e1d310ce19b0a818e7940e52ea5131
Yose216 <youness.chetoui@gmail.com>
2017-08-30 17:28:39 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'bdd.php'; //Connect to BDD
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+	
+		<title>Test POO php</title>	
+        <link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+
+		<h1>Test POO</h1>
+
+	</body>
+</html>
